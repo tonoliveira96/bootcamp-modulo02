ddb6498e1bebd26265f0c50ec0a564783ad7507a
Everton <tonn.oliveira2011@gmail.com>
2020-02-08 21:06:28 -0300

    adicionado autenticação

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e514d6fbe5d0567d7648755f339514bd',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,12 @@
 /* eslint-disable linebreak-style */
 import { Router } from 'express';
-import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-
-routes.get('/users', (req, res) => {
-  res.json({ message: 'Opaaaaa!' });
-});
+routes.post('/session', SessionController.store);
 
 export default routes;
