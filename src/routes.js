e26a9d75436a4f83f0838f6410e48efca5cca6e2
Everton <tonn.oliveira2011@gmail.com>
2020-02-12 22:37:05 -0300

    adicionado upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 /* eslint-disable linebreak-style */
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
@@ -14,4 +18,6 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
