f9d1339bea4044e00955d35cf6757e02f47d5f57
Everton <tonn.oliveira2011@gmail.com>
2020-02-15 19:06:47 -0300

    cria agendamentos

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -19,6 +21,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
