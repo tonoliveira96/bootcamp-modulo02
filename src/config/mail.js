24b0bf97608e18ff21d680a8cd209197ae2df0d0
Everton <tonn.oliveira2011@gmail.com>
2020-02-20 21:19:31 -0300

    configurado Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4de50a9619e888',
+    pass: '038aa77d993d0c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com.br>',
+  },
+};
